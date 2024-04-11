cdb692a5f662a69ae48dceffb81b6a9dde7ba15b
FRIASMIGUEL <jmfrias1950@gmail.com>
2024-04-11 10:16:59 -0300

    Documento formatado e .gitgnore

--- cypress/e2e/exercicio-api.cy.js
+++ cypress/e2e/exercicio-api.cy.js
@@ -5,104 +5,104 @@ import { faker } from '@faker-js/faker'
 import contrato from '../contracts/usuarios.contract'
 
 describe('Testes da Funcionalidade Usuários', () => {
-let token
-before(() => {
-cy.token('fulano@qa.com', 'teste').then(tkn => { token = tkn })
-})
+    let token
+    before(() => {
+        cy.token('fulano@qa.com', 'teste').then(tkn => { token = tkn })
+    })
 
-it('Deve validar contrato de usuários', () => {
+    it('Deve validar contrato de usuários', () => {
 
-cy.request('usuarios').then(response => {
-return contrato.validateAsync(response.body)
-})
-})
+        cy.request('usuarios').then(response => {
+            return contrato.validateAsync(response.body)
+        })
+    })
 
-it('Deve listar usuários cadastrados', () => {
+    it('Deve listar usuários cadastrados', () => {
 
-cy.request({
-method: 'GET',
-url: 'usuarios',
+        cy.request({
+            method: 'GET',
+            url: 'usuarios',
 
-}).then((response) => {
-// expect(response.body.usuarios[0].nome).to.equal('Fulano da Silva')
-expect(response.status).to.equal(200);
-})
-});
+        }).then((response) => {
+            // expect(response.body.usuarios[0].nome).to.equal('Fulano da Silva')
+            expect(response.status).to.equal(200);
+        })
+    });
 
-it('Deve cadastrar um usuário com sucesso', () => {
+    it('Deve cadastrar um usuário com sucesso', () => {
 
-let nome = faker.internet.userName()
-let email = faker.internet.email()
-let senha = faker.internet.password()
+        let nome = faker.internet.userName()
+        let email = faker.internet.email()
+        let senha = faker.internet.password()
 
-cy.cadastrarusuario(nome, email, senha)
+        cy.cadastrarusuario(nome, email, senha)
 
-.then(response => {
-expect(response.status).to.equal(201)
-expect(response.body.message).to.equal('Cadastro realizado com sucesso')
-})
-})
+            .then(response => {
+                expect(response.status).to.equal(201)
+                expect(response.body.message).to.equal('Cadastro realizado com sucesso')
+            })
+    })
 
-it('Deve validar um usuário com email inválido', () => {
+    it('Deve validar um usuário com email inválido', () => {
 
-cy.cadastrarusuario("Abrantes", "amq@a.com", "senha")
+        cy.cadastrarusuario("Abrantes", "amq@a.com", "senha")
 
-.then(response => {
-expect(response.status).to.equal(400)
-expect(response.body.message).to.equal('Este email já está sendo usado')
-})
-})
+            .then(response => {
+                expect(response.status).to.equal(400)
+                expect(response.body.message).to.equal('Este email já está sendo usado')
+            })
+    })
 
-it('Deve editar um usuário previamente cadastrado', () => {
+    it('Deve editar um usuário previamente cadastrado', () => {
 
-let nome = faker.internet.userName()
-let email = faker.internet.email()
-let senha = faker.internet.password()
+        let nome = faker.internet.userName()
+        let email = faker.internet.email()
+        let senha = faker.internet.password()
 
-cy.cadastrarusuario(nome, email, senha)
+        cy.cadastrarusuario(nome, email, senha)
 
-.then(response => {
-let id = response.body._id
+            .then(response => {
+                let id = response.body._id
 
-cy.request({
-method: 'PUT',
-url: `usuarios/${id}`,
-body: {
-"nome": 'nome alterado',
-"email": email,
-"password": senha,
-"administrador": "true",
-}
-})
-.then((response) => {
-expect(response.status).to.equal(200)
-expect(response.body.message).to.equal('Registro alterado com sucesso')
-})
-})
-})
+                cy.request({
+                    method: 'PUT',
+                    url: `usuarios/${id}`,
+                    body: {
+                        "nome": 'nome alterado',
+                        "email": email,
+                        "password": senha,
+                        "administrador": "true",
+                    }
+                })
+                    .then((response) => {
+                        expect(response.status).to.equal(200)
+                        expect(response.body.message).to.equal('Registro alterado com sucesso')
+                    })
+            })
+    })
 
-it('Deve deletar um usuário previamente cadastrado', () => {
+    it('Deve deletar um usuário previamente cadastrado', () => {
 
-let nome = faker.internet.userName()
-let email = faker.internet.email()
-let senha = faker.internet.password()
+        let nome = faker.internet.userName()
+        let email = faker.internet.email()
+        let senha = faker.internet.password()
 
-cy.cadastrarusuario(nome, email, senha)
+        cy.cadastrarusuario(nome, email, senha)
 
-.then(response => {
-let id = response.body._id
+            .then(response => {
+                let id = response.body._id
 
-cy.request({
-method: 'DELETE',
-url: `usuarios/${id}`,
-}
-)
-})
-.then((response) => {
-expect(response.status).to.equal(200)
-expect(response.body.message).to.have.equal('Registro excluído com sucesso')
-})
-})
+                cy.request({
+                    method: 'DELETE',
+                    url: `usuarios/${id}`,
+                }
+                )
+            })
+            .then((response) => {
+                expect(response.status).to.equal(200)
+                expect(response.body.message).to.have.equal('Registro excluído com sucesso')
+            })
+    })
 })
 
 
